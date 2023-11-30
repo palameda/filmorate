efbe22df82557de72c5125e2fd6184058d8b5851
palameda <stanislav.s.mazurenko@yandex.ru>
2023-11-30 11:30:00 +0300

    doc: edit documentation

--- src/main/java/ru/practicum/javafilmorate/utils/InvalidDataExcepion.java
+++ src/main/java/ru/practicum/javafilmorate/utils/InvalidDataExcepion.java
@@ -1,5 +1,8 @@
 package ru.practicum.javafilmorate.utils;
 
+/**
+ * InvalidDataException - runtime-исключение, которое генерируется в случае обработки некорректных данных
+ */
 public class InvalidDataExcepion extends RuntimeException {
     public InvalidDataExcepion(String message) {
         super(message);
