55c3008e8ac2c37173974dc7421be7c792a324df
palameda <stanislav.s.mazurenko@yandex.ru>
2023-12-13 15:33:39 +0300

    refactor: move film-methods declarations to FilmStorage interface

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/FilmStorage.java
@@ -1,4 +1,15 @@
 package ru.yandex.practicum.javafilmorate.storage;
 
+import ru.yandex.practicum.javafilmorate.model.Film;
+
+import java.util.Map;
+
 public interface FilmStorage {
+    Film addFilm(Film film);
+
+    Film updateFilm(Film film);
+
+    void deleteFilm(Film film);
+
+    Map<Integer, Film> getAllFilms();
 }
