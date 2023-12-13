eb0e8b1e017902c8193a5c99b9d016c27f90c586
palameda <stanislav.s.mazurenko@yandex.ru>
2023-12-13 15:48:32 +0300

    feat: implement methods of the FilmStorage interface

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/InMemoryFilmStorage.java
@@ -1,4 +1,37 @@
 package ru.yandex.practicum.javafilmorate.storage;
 
-public class InMemoryFilmStorage {
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.javafilmorate.model.Film;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int id = 1;
+
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(id);
+        id++;
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public void deleteFilm(Film film) {
+        films.remove(film.getId());
+    }
+
+    @Override
+    public Map<Integer, Film> getAllFilms() {
+        return new HashMap<>(films);
+    }
 }
