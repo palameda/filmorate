c9fb11fe09ad90a615eca50f7c08d517b14ea9a6
palameda <stanislav.s.mazurenko@yandex.ru>
2023-12-13 17:28:59 +0300

    feat: add likes property to film entity

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Film.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Data-класс Film является моделью сущности "Фильм" в сервисе Filmorate и содержит поля:
@@ -36,4 +38,5 @@ public class Film {
     private final LocalDate releaseDate;
     @Positive
     private final int duration;
+    private final Set<Integer> likes = new HashSet<>();
 }
