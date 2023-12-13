8b4442d47788ccd33e71c867d699624cd5e7250b
palameda <stanislav.s.mazurenko@yandex.ru>
2023-12-13 19:57:17 +0300

    refactor: change request managment using service logic

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/UserController.java
@@ -1,49 +1,87 @@
 package ru.yandex.practicum.javafilmorate.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.javafilmorate.model.User;
-import ru.yandex.practicum.javafilmorate.utils.InvalidDataExcepion;
+import ru.yandex.practicum.javafilmorate.service.UserService;
 import ru.yandex.practicum.javafilmorate.utils.InvalidDataException;
+import ru.yandex.practicum.javafilmorate.utils.UnregisteredDataException;
 
-import java.util.Map;
-import java.util.HashMap;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Класс UserController работает с endpoint'ом "/users" и обслуживает пользователей сервиса Filmorate.
- * Выполняет обработку POST, PUT и GET -запросов.
+ * Выполняет обработку GET, POST, PUT и DELETE -запросов.
  */
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
+@AllArgsConstructor
 public class UserController {
-    private int id = 1;
-    private final Map<Integer, User> users = new HashMap<>();
+    private final UserService userService;
 
     /**
-     * Метод createUser обрабатывает POST-запрос и добавляет пользователя в систему.<br>
-     * В случае некорректных данных генерируется исключение InvalidDataException.
+     * Метод findAll обрабатывает GET-запрос и возвращает список пользователей, зарегистрированных в системе
+     * @return ArrayList<User>()
+     */
+    @GetMapping
+    public List<User> findAll() {
+        return userService.getAllUsers();
+    }
+
+    /**
+     * Метод getUserById обрабатывает GET-запрос и возвращает пользователя с указанным id
+     * @param id
+     * @return User
+     */
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    /**
+     * Метод findAllFriends обрабатывает GET-запрос и возвращает список друзей пользователя с указанным id
+     * @param id
+     * @return ArrayList<User>()
+     */
+    @GetMapping("/{id}/friends")
+    public List<User> findAllFriends(@PathVariable int id) {
+        return userService.getUserFriends(id);
+    }
+
+    /**
+     * Метод findCommonFriends обрабатывает GET-запрос и возвращает список общих друзей
+     * для пользователей с указанными id и otherId
+     * @param id
+     * @param otherId
+     * @return ArrayList<User>()
+     */
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> findCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+        return userService.getCommonFriends(id, otherId);
+    }
+
+    /**
+     * Метод createUser обрабатывает POST-запрос и добавляет пользователя в систему.
      * @param user
      * @return user
      */
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        if (user.getLogin().contains(" ")) {
-            log.error("При выполнении POST-запроса передан login пользователя, содержащий пробел(ы)");
-            throw new InvalidDataException("login пользователя не должен содержать пробелы");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        user.setId(id);
-        users.put(user.getId(), user);
-        id++;
-        log.info("Пользователь {} ({}) успешно добавлен", user.getLogin(), user.getName());
-        return user;
+        return userService.addUser(user);
+    }
+
+    /**
+     * Метод addFriend обрабатывает PUT-запрос и делает "друзьями" пользователей с id и friendId
+     * @param id
+     * @param friendId
+     */
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        userService.addFriend(id, friendId);
     }
 
     /**
@@ -54,29 +92,29 @@ public class UserController {
      */
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        if (!users.containsKey(user.getId())) {
-            log.error("При выполнении PUT-запроса передан пользователь, который не зарегистрирован в системе");
-            throw new InvalidDataException("Пользователь не зарегистрирован в системе");
-        }
-        if (user.getLogin().contains(" ")) {
-            log.error("При выполнении PUT-запроса передан login пользователя, содержащий пробел(ы)");
-            throw new InvalidDataException("login пользователя не должен содержать пробелы");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        users.put(user.getId(), user);
-        log.info("Данные пользователя {} ({}) успешно обновлены", user.getLogin(), user.getName());
-        return user;
+        return userService.updateUser(user);
     }
 
     /**
-     * Метод findAll обрабатывает GET-запрос и возвращает список пользователей, зарегистрированных в системе
-     * @return ArrayList<>(users)
+     * Метод deleteFriend обрабатывает DELETE-запрос и удаляет из списка друзей пользователя с id
+     * другого пользователя системы с friendId
+     * @param id
+     * @param friendId
      */
-    @GetMapping
-    public List<User> findAll() {
-        log.info("При выполнении GET-запроса получено пользователей: {}", users.size());
-        return new ArrayList<>(users.values());
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        userService.deleteFriend(id, friendId);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleUnregisteredDataException(final UnregisteredDataException exception) {
+        return Map.of("errorMessage", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidDataException(final InvalidDataException exception) {
+        return Map.of("errorMessage", exception.getMessage());
     }
 }
