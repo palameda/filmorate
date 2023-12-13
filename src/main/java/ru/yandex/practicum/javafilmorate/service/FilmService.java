f2953fe2786500d77028d868e5e18775cb315a52
palameda <stanislav.s.mazurenko@yandex.ru>
2023-12-13 16:01:42 +0300

    feat: move film-methods declaration to FilmService interface

--- src/main/java/ru/yandex/practicum/javafilmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/service/FilmService.java
@@ -1,4 +1,25 @@
 package ru.yandex.practicum.javafilmorate.service;
 
-public class FilmService {
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.javafilmorate.model.Film;
+
+import java.util.List;
+
+@Service
+public interface FilmService {
+    Film addFilm(Film film);
+
+    Film updateFilm(Film film);
+
+    void deleteFilm(Film film);
+
+    Film getFilmById(int id);
+
+    List<Film> getFilms();
+
+    void addLike(int id, int userId);
+
+    void deleteLike(int id, int userId);
+
+    List<Film> getPopularFilms(String count);
 }
