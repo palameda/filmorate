2d65e5850c0f720c2a80654bf61716a694d8570e
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-11 21:20:16 +0300

    refactor: add validation for class fields

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Genre.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.javafilmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class Genre {
     private final int id;
+    @NotBlank(message = "Название жанра не может быть пустым")
     private final String name;
 }
