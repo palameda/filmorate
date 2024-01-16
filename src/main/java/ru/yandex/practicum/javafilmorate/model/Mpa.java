07df1c1870ffe47ef84d907daeb83757b6e13e1c
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-16 16:42:11 +0300

    refactor: remove description property that is not used in Postman tests

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Mpa.java
@@ -7,8 +7,7 @@ import javax.validation.constraints.Size;
 
 @Data
 public class Mpa {
+    private final int id;
     @NotBlank(message = "Наименование рейтинга (MPA) не может быть пустым")
     private final String rating;
-    @Size(max = 200, message = "Описание рейтинга (MPA) не может превышать 200 символов")
-    private final String description;
 }
