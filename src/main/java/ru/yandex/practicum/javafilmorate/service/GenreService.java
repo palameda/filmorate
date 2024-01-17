5b4d63e6ac8042c571fac58347652328b1ccb4f8
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-17 14:43:12 +0300

    feat: add genre service

--- src/main/java/ru/yandex/practicum/javafilmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/service/GenreService.java
@@ -1,2 +1,26 @@
-package ru.yandex.practicum.javafilmorate.service;public interface GenreService {
+package ru.yandex.practicum.javafilmorate.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.javafilmorate.model.Genre;
+import ru.yandex.practicum.javafilmorate.storage.dao.GenreStorage;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class GenreService {
+    private final GenreStorage genreStorage;
+
+    public List<Genre> getFilmGenres(int filmId) {
+        return genreStorage.getGenres(filmId);
+    }
+
+    public Genre getById(int id) {
+        return new Genre(id, genreStorage.findById(id));
+    }
+
+    public List<Genre> getAll() {
+        return genreStorage.findAll();
+    }
 }
