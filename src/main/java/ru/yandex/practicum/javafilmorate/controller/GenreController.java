28c6be6a5495e30e631f4f423ed6f43e4c2f5875
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-17 14:43:29 +0300

    feat: add genre controller

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/GenreController.java
@@ -1,4 +1,35 @@
 package ru.yandex.practicum.javafilmorate.controller;
 
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.javafilmorate.model.Genre;
+import ru.yandex.practicum.javafilmorate.service.GenreService;
+
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/genres")
 public class GenreController {
+    private final GenreService genreService;
+
+    @GetMapping()
+    public List<Genre> getGenres() {
+        log.info("Получение списка жанров");
+        return genreService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Genre> getGenreById(@PathVariable Integer id) {
+        log.info("Получение жанра из базы данных по id = {}", id);
+        return new ResponseEntity<>(genreService.getById(id), HttpStatus.OK);
+    }
 }
