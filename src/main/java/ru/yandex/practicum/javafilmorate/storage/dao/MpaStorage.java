0e464db51df3ec58cce98bbc35b1a6ba49e9ff68
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-17 16:08:28 +0300

    feat: add mpa storage interface

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/MpaStorage.java
@@ -1,4 +1,13 @@
 package ru.yandex.practicum.javafilmorate.storage.dao;
 
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.javafilmorate.model.Mpa;
+
+import java.util.List;
+
+@Component
 public interface MpaStorage {
+    String findById(int id);
+
+    List<Mpa> findAll();
 }
