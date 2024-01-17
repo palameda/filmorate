f292825ffca975500604b8d4c937a40e3f83ba55
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-17 14:10:27 +0300

    fix: add constructor for non-final properties

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Genre.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.javafilmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
 public class Genre {
-    private final int id;
+    private int id;
     @NotBlank(message = "Название жанра не может быть пустым")
-    private final String name;
+    private String name;
 }
