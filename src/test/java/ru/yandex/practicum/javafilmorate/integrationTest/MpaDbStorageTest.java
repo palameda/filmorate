15a010cc67282aab72af8c20e55efd12f0a289fe
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-18 16:23:16 +0300

    test: test mpa db storage methods

--- src/test/java/ru/yandex/practicum/javafilmorate/integrationTest/MpaDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/javafilmorate/integrationTest/MpaDbStorageTest.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.javafilmorate.controller;
+package ru.yandex.practicum.javafilmorate.integrationTest;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.javafilmorate.JavaFilmorateApplication;
 import ru.yandex.practicum.javafilmorate.storage.dao.implementation.MpaDbStorage;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 @SpringBootTest(classes = JavaFilmorateApplication.class)
 @AutoConfigureTestDatabase
@@ -18,21 +19,22 @@ import java.util.LinkedList;
 class MpaDbStorageTest {
     private final MpaDbStorage mpaDbStorage;
     @Test
-    @DisplayName("Проверка получения MPA по id")
-    void testFindNameMpa() {
-        LinkedList<String> nameMpa = new LinkedList<>();
-        nameMpa.add("G");
-        nameMpa.add("PG");
-        nameMpa.add("PG-13");
-        nameMpa.add("R");
-        nameMpa.add("NC-17");
-        for (int i = 1; i < nameMpa.size()-1; i++) {
-            Assertions.assertEquals(mpaDbStorage.findById(i), nameMpa.get(i-1), "Названия рейтингов не совпадают");
+    @DisplayName("Проверка метода findById для Mpa")
+    void testFindMpaById() {
+        ArrayList<String> mpa = new ArrayList<>();
+        mpa.add("G");
+        mpa.add("PG");
+        mpa.add("PG-13");
+        mpa.add("R");
+        mpa.add("NC-17");
+        for (int i = 1; i < mpa.size()-1; i++) {
+            Assertions.assertEquals(mpaDbStorage.findById(i), mpa.get(i-1), "Названия рейтингов не совпадают");
         }
     }
 
     @Test
-    void testFindAll() {
-        Assertions.assertEquals(5, mpaDbStorage.findAll().size(), "Размер не соответсвует");
+    @DisplayName("Проверка метода findAll() для Mpa")
+    void testFindAllMpa() {
+        Assertions.assertEquals(5, mpaDbStorage.findAll().size(), "Количество данных не совпадает");
     }
 }
