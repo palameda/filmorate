f09b2621ba8392a93782ecebaade6b6f474bbca1
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-20 00:02:20 +0300

    feat: add like storage

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/LikeStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/LikeStorage.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.javafilmorate.storage.dao;
 
-public class LikeStorage {
+import java.util.List;
+
+public interface LikeStorage {
+    void addLike(Integer filmId, Integer userId);
+
+    void deleteLike(int filmId, int userId);
+
+    List<Integer> getLikes(int filmId);
 }
