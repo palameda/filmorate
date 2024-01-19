d3ba81c2266bae1044d6c5515761920f9e852f89
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-20 00:00:42 +0300

    refactor: simplify user storage interface

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/UserStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/UserStorage.java
@@ -4,21 +4,16 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.javafilmorate.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public interface UserStorage {
-    Integer add(User user);
+    User add(User user);
 
-    void update(User user);
+    User update(User user);
 
-    Optional<User> findById(int userId);
+    User delete(User user);
+
+    User findById(int userId);
 
     List<User> findAll();
-
-    boolean addFriendRequest(int userId, int friendId);
-
-    List<Integer> findAllFriends(int userId);
-
-    boolean deleteFriends(int userId, int friendId);
 }
