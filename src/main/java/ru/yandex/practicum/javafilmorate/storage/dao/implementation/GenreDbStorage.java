e6d90f093d0487f7592c3dffcbc09c47a7990445
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-20 00:08:36 +0300

    refactor: rework genre storage interface implementation

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/implementation/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/implementation/GenreDbStorage.java
@@ -3,13 +3,16 @@ package ru.yandex.practicum.javafilmorate.storage.dao.implementation;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.javafilmorate.model.Film;
 import ru.yandex.practicum.javafilmorate.model.Genre;
 import ru.yandex.practicum.javafilmorate.storage.dao.GenreStorage;
 import ru.yandex.practicum.javafilmorate.utils.UnregisteredDataException;
 
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,37 +24,70 @@ public class GenreDbStorage implements GenreStorage {
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public List<Genre> getGenres(int filmId) {
-        List<Genre> genres = new ArrayList<>();
-        String sqlQuery = String.format("SELECT GENRE_ID FROM FILM_GENRES WHERE FILM_ID = %d", filmId);
-        List<Integer> genreIds = jdbcTemplate.queryForList(sqlQuery, Integer.class);
-        for (Integer id : genreIds) {
-            genres.add(new Genre(id, findById(id)));
+    public Genre findById(int genreId) {
+        log.info("Получение жанра по id {}", genreId);
+        SqlRowSet rs = jdbcTemplate.queryForRowSet("SELECT * FROM GENRES WHERE GENRE_ID = ?", genreId);
+        if (rs.next()) {
+            return new Genre(
+                    rs.getInt("GENRE_ID"),
+                    rs.getString("GENRE_NAME")
+            );
+        } else {
+            throw new UnregisteredDataException("Жанр с id " + genreId + " не зарегистрирован в системе");
         }
-        log.info("Получение жанров для фильма");
-        return genres;
-    }
-
-    @Override
-    public String findById(int id) {
-        String sqlQuery = String.format("SELECT GENRE_NAME FROM GENRES WHERE GENRE_ID = %d", id);
-        List<String> genreNames = jdbcTemplate.queryForList(sqlQuery, String.class);
-        if (genreNames.size() != 1) {
-            throw new UnregisteredDataException("Передан некорректный id жанра");
-        }
-        log.info("Получение жанра по id {}", id);
-        return genreNames.get(0);
     }
 
     @Override
     public List<Genre> findAll() {
-        String sqlQuery = "SELECT GENRE_ID, GENRE_NAME FROM GENRES";
-        log.info("Получение списка жанров");
-        return jdbcTemplate.query(sqlQuery, this::rowMapper);
+        log.info("Получение из хранилища списка всех жанров");
+        List<Genre> genres = new ArrayList<>();
+        SqlRowSet rs = jdbcTemplate.queryForRowSet("SELECT * FROM GENRES");
+        while (rs.next()) {
+            Genre genre = new Genre(
+                    rs.getInt("GENRE_ID"),
+                    rs.getString("GENRE_NAME")
+            );
+            genres.add(genre);
+        }
+        return genres;
     }
 
-    private Genre rowMapper(ResultSet rs, int rowNum) throws SQLException {
-        log.info("Производится маппинг жанра");
-        return new Genre(rs.getInt("GENRE_ID"), rs.getString("GENRE_NAME"));
+    @Override
+    public void reloadGenres(Film film) {
+        log.info("Перезапись-обновление жанров для фильма с {}", film.getName());
+        deleteFilmGenre(film);
+        addFilmGenre(film);
+    }
+
+    @Override
+    public void deleteFilmGenre(Film film) {
+        log.info("Удаление жанров для фильма {}", film.getName());
+        jdbcTemplate.update("DELETE FROM FILM_GENRES WHERE FILM_ID = ?", film.getId());
+    }
+
+    @Override
+    public void addFilmGenre(Film film) {
+        List<Genre> genres = film.getGenres();
+        if (!genres.isEmpty()) {
+            String sqlQuery = "MERGE INTO FILM_GENRES (FILM_ID, GENRE_ID) VALUES (?, ?)";
+            jdbcTemplate.batchUpdate(sqlQuery, new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    ps.setInt(1, film.getId());
+                    ps.setInt(2, genres.get(i).getId());
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return genres.size();
+                }
+            });
+        }
+//        if (film.getGenres() != null) {
+//            for (Genre genre : film.getGenres()) {
+//                jdbcTemplate.update("INSERT INTO film_genres (film_id, genre_id) VALUES (?, ?)",
+//                        film.getId(), genre.getId());
+//            }
+//        }
     }
 }
