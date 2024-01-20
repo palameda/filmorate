baf5aad69df1ba52aa85310db52e5d0520b6a438
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-20 21:10:07 +0300

    fix: change type from int to Integer to handle null values

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Film.java
@@ -14,7 +14,7 @@ import java.util.Set;
 
 @Data
 public class Film {
-    private int id;
+    private Integer id;
     @NotBlank(message = "Название фильма не может быть пустым")
     private final String name;
     @Size(max = 200, message = "Размер описания не должен превышать 200 символов")
@@ -22,13 +22,13 @@ public class Film {
     @ReleaseDateValidation(message = "Дата показа не может предшествовать 28 декабря 1895 года")
     private final LocalDate releaseDate;
     @Positive(message = "Продолжительнось фильма должна быть больше 0")
-    private final int duration;
-    private Mpa mpa;
-    private int likes;
+    private final Integer duration;
+    private final Mpa mpa;
+    private final int likes;
     private Set<Genre> genres = new HashSet<>();
 
     public Film(Integer id, String name, String description, LocalDate releaseDate,
-                int duration, Mpa mpa, int likes) {
+                Integer duration, Mpa mpa, int likes) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -38,7 +38,7 @@ public class Film {
         this.likes = likes;
     }
 
-    public Map<String, Object> filmRowMap() {
+    public Map<String, Object> filmToMap() {
         Map<String, Object> values = new HashMap<>();
         values.put("FILM_NAME", name);
         values.put("FILM_DESCRIPTION", description);
