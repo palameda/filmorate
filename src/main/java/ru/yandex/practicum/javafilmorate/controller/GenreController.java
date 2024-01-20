c1e9d7b70b920b2e6308e0fb0208ed3984d373a7
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-20 10:25:56 +0300

    refactor: rework genre contorller

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/GenreController.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.javafilmorate.controller;
 
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +11,6 @@ import ru.yandex.practicum.javafilmorate.service.GenreService;
 
 import java.util.List;
 
-@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/genres")
@@ -22,14 +18,12 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping()
-    public List<Genre> getGenres() {
-        log.info("Получение списка жанров");
-        return genreService.getAll();
+    public List<Genre> findAll() {
+        return genreService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Genre> getGenreById(@PathVariable Integer id) {
-        log.info("Получение жанра из базы данных по id = {}", id);
-        return new ResponseEntity<>(genreService.getById(id), HttpStatus.OK);
+    public Genre findById(@PathVariable Integer id) {
+        return genreService.findById(id);
     }
 }
