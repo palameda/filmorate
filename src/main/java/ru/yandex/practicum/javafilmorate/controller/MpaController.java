3531b8dc6d4b1cd0c9cf20a4bc1bdc3c2f71675a
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-20 10:29:53 +0300

    refactor: rework genre contorller

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/MpaController.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.javafilmorate.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +10,6 @@ import ru.yandex.practicum.javafilmorate.service.MpaService;
 
 import java.util.List;
 
-@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/mpa")
@@ -21,14 +17,12 @@ public class MpaController {
     private final MpaService mpaService;
 
     @GetMapping()
-    public List<Mpa> getAll() {
-        log.info("Получение списка рейтингов");
-        return mpaService.getAll();
+    public List<Mpa> findAll() {
+        return mpaService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Mpa> getById(@PathVariable Integer id) {
-        log.info("Получение рейтинга из базы данных по id = {}", id);
-        return new ResponseEntity<>(mpaService.getById(id), HttpStatus.OK);
+    public Mpa findById(@PathVariable Integer id) {
+        return mpaService.findById(id);
     }
 }
