fd57ab8cb07cf246573235ac7076c2bb37eb6d0b
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-29 23:42:43 +0300

    feat: добавить интерфейс с методами для работы с хранилищем данных по режиссерам

---
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/DirectorDbStorage.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.javafilmorate.storage.dao;
+
+import ru.yandex.practicum.javafilmorate.model.Director;
+
+import java.util.List;
+
+public interface DirectorDbStorage {
+
+    List<Director> findAll();
+
+    Director findById(Integer directorId);
+
+    Integer addDirector(Director director);
+
+    void updateDirector(Director director);
+
+    void deleteDirector(Integer directorId);
+
+}
