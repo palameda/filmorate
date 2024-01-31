5020da1caec42921c13eeb5259b646383bab4263
palameda <stanislav.s.mazurenko@yandex.ru>
2024-01-31 13:22:17 +0300

    refactor: добавить к классу аннотацию AllArgsConstructor

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Director.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.javafilmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
 public class Director {
     private Integer id;
     @NotBlank(message = "Имя режиссера не может быть пустым")
