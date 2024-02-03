37a93ba0ea85f91433a53745c9b448f35dd41f5b
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-03 16:07:56 +0300

    style: выполнить проверку импортов

--- src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/EventStorage.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/storage/dao/EventStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.javafilmorate.storage.dao;
 
 import ru.yandex.practicum.javafilmorate.model.Event;
-
 import java.util.List;
 
 public interface EventStorage {
@@ -9,6 +8,4 @@ public interface EventStorage {
     Event add(Event event);
 
     List<Event> getUserEvents(int userId);
-
-
 }
