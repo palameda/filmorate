4936ce98f3631fee9ad631f3e2639ecfdaf7de4a
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-06 09:23:10 +0300

    style: удалить лишний импорт

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/ReviewController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.javafilmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.javafilmorate.model.Review;
 import ru.yandex.practicum.javafilmorate.service.ReviewService;
